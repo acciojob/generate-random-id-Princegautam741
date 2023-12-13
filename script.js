e2e2230599336aaad83518205b745483b4485aae
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-13 16:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// script.js
 function makeid(l) {
-  // write your code here
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < l; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+const length = prompt("Enter a number.");
+alert(makeid(length));
